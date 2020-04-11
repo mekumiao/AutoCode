1dbca1f94f52625f3a44febcfc834cec2d1f4630
qiqigou <2319002545@qq.com>
2020-04-11 15:30:30 +0800

    添加项目文件。

---
+++ AutoCode/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TempTop;
+
+namespace AutoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Create();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"发生异常:{ex.Message}!!!");
+            }
+            Console.Write("按任意键退出!!!");
+            Console.Read();
+        }
+
+        public static void Create()
+        {
+            var set = new SettingManger().Setting;
+            var tempdict = new Dictionary<string, ITempBuild>();
+            var build = default(ITempBuild);
+
+            //编译模板
+            foreach (var item in set.data)
+            {
+                var temppath = Path.Combine(set.tempdir, item.tempname);
+
+                if (!File.Exists(temppath)) throw new Exception($"模板文件不存在:{temppath}");
+
+                build = new TempManager(temppath).GetTempBuild();
+                Console.WriteLine($"模板编译成功! {temppath}");
+                tempdict.Add(item.tempname, build);
+            }
+
+            //全局输出文件夹是否存在
+            if (!Directory.Exists(set.outdir))
+                Directory.CreateDirectory(set.outdir);
+
+            //输出结果
+            foreach (var item in set.data)
+            {
+                foreach (var dtname in item.dataname)
+                {
+                    var datapath = Path.Combine(set.datadir, dtname);
+                    bool issuccess = tempdict.TryGetValue(item.tempname, out build);
+                    if (issuccess)
+                    {
+                        if (!File.Exists(datapath))
+                        {
+                            Console.WriteLine($"数据文件不存在:{datapath}");
+                            break;
+                        }
+                        //加载数据
+                        build.LoadFromFile(datapath);
+                        Console.WriteLine($"数据加载成功! {datapath}");
+                        //生成代码
+                        var code = build.Execute();
+                        var outpath = Path.Combine(item.outdir, string.Format(item.filename, Path.GetFileNameWithoutExtension(dtname)));
+                        using (var writer = new StreamWriter(outpath))
+                        {
+                            writer.Write(code);
+                            Console.WriteLine($"代码生成成功! {outpath}");
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ AutoCode/SettingManger.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCode
+{
+    internal class SettingManger
+    {
+        internal setting Setting { get; private set; }
+
+        internal SettingManger()
+        {
+            var path = "./setting.json";
+            using (var reader = new StreamReader(path))
+            {
+                var jsonstr = reader.ReadToEnd();
+                Setting = JsonConvert.DeserializeObject<setting>(jsonstr);
+                var list = Setting.data.Where(x => string.IsNullOrWhiteSpace(x.outdir) || !Directory.Exists(x.outdir)).ToList();
+                if (list != null || list.Any())
+                {
+                    list?.ForEach(x => x.outdir = Setting.outdir);
+                }
+            }
+        }
+
+
+        public class setting
+        {
+            public string tempdir { get; set; }
+
+            public string datadir { get; set; }
+
+            public string outdir { get; set; }
+
+            public List<data> data { get; set; }
+        }
+
+        public class data
+        {
+            public string tempname { get; set; }
+
+            public string filename { get; set; }
+
+            public string outdir { get; set; }
+
+            public List<string> dataname { get; set; }
+        }
+
+    }
+}
